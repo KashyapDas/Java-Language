208ad16fe7d82a5a0740a94d2f058b9ae3645877
Kashyap Das <kashyap.education2234@gmail.com>
2025-08-17 22:21:18 +0530

    doing some changes in the DataTypes file

--- in/DataTypes.java
+++ in/DataTypes.java
@@ -5,11 +5,17 @@ public class DataTypes {
     {
         String name = "Kashyap";
         int age = 30;
+        long year = 2004L;
+        float rank = 82F;
+        double marks = 34.5D;
         boolean isElligible = true;
 
         System.out.println(name);
         System.out.println(age);
         System.out.println(isElligible);
+        System.out.println(year);
+        System.out.println(rank);
+        System.out.println(marks);
 
     }
 }
