f098dad0a684f248ecfb4c1f14fbfa6658214a76
Kashyap Das <kashyap.education2234@gmail.com>
2025-08-17 22:15:09 +0530

    intrduce the java repo in the github

---
+++ in/DataTypes.java
@@ -0,0 +1,15 @@
+package in;
+
+public class DataTypes {
+    public static void main(String[] args)
+    {
+        String name = "Kashyap";
+        int age = 30;
+        boolean isElligible = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(isElligible);
+
+    }
+}
---
+++ in/HelloWorld.java
@@ -0,0 +1,8 @@
+package in;
+
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
