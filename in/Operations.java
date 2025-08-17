16828213fb8998c81b2b3cbff5dedc6486e0c417
Kashyap Das <kashyap.education2234@gmail.com>
2025-08-17 22:24:19 +0530

    add another new file

---
+++ in/Operations.java
@@ -0,0 +1,14 @@
+package in;
+
+public class Operations {
+    public static void main(String[] args)
+    {
+        String firstName = "Kashyap";
+        String middleName = "Jyoti";
+        String lastname = "Das";
+
+        String name = firstName + " " + middleName + " " + lastname ;
+    
+        System.out.print("Your name is "+name);
+    }
+}
