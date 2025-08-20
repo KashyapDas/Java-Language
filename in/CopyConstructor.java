7c0f322b7110d299be1bb78ef77aceeae8035bd3
Kashyap Das <kashyap.education2234@gmail.com>
2025-08-20 06:04:36 +0530

    add new file - concept : copyConstructor and one readme file for the concept of destructor

---
+++ in/CopyConstructor.java
@@ -0,0 +1,31 @@
+package in;
+
+class OurStudent{
+    String name;
+    int age;
+    String location; 
+
+    OurStudent(){}
+
+    OurStudent(OurStudent o1)
+    {
+        this.name = o1.name;
+        this.age = o1.age;
+        this.location = o1.location;
+    }
+
+}
+
+
+public class CopyConstructor{
+    @SuppressWarnings("unused")
+    public static void main(String args[])
+    {
+        OurStudent o1 = new OurStudent();
+        o1.name = "Kashyap";
+        o1.age = 22;
+        o1.location = "Assam";
+
+        OurStudent o2 = new OurStudent(o1);
+    }
+}
