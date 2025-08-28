33517ddde8b62066343a7c8230cae9fecebe622e
Kashyap Das <kashyap.education2234@gmail.com>
2025-08-28 20:25:26 +0530

    add 2 files

---
+++ in/ForLoop.java
@@ -0,0 +1,22 @@
+package in;
+
+import java.util.Scanner;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = {1,2,3,4,5};
+
+        int sum = 0;
+        for (int num : arr) 
+        {   // enhanced for loop
+            sum += num;
+        }
+
+        // Print the sum 
+        System.out.println("Sum : " + sum);
+
+        sc.close();
+    }
+}
